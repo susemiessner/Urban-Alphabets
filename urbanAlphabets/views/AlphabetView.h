d5595b78daf9763eb3456c2411edb9b624f8e1d6
susemiessner <susanne.miessner@gmail.com>
2013-10-25 15:17:46 +0300

    forwarding default variables from C4Workspace
    working version so far

---
+++ urbanAlphabets/views/AlphabetView.h
@@ -0,0 +1,13 @@
+//
+//  AlphabetView.h
+//  urbanAlphabets
+//
+//  Created by SuseMiessner on 10/25/13.
+//  Copyright (c) 2013 SuseMiessner. All rights reserved.
+//
+
+#import "C4CanvasController.h"
+
+@interface AlphabetView : C4CanvasController
+
+@end
---
+++ urbanAlphabets/views/AlphabetView.m
@@ -0,0 +1,38 @@
+//
+//  AlphabetView.m
+//  urbanAlphabets
+//
+//  Created by SuseMiessner on 10/25/13.
+//  Copyright (c) 2013 SuseMiessner. All rights reserved.
+//
+
+#import "AlphabetView.h"
+
+@interface AlphabetView ()
+
+@end
+
+@implementation AlphabetView
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
