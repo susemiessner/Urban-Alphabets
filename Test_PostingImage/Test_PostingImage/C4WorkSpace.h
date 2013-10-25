3301195f1d0f3135e9c34c37e605bb36da74dbb2
susemiessner <susanne.miessner@gmail.com>
2013-10-25 11:40:23 +0300

    Test Posting Image between subviews
    still not working, even though the image seems to arrive in the new
    view….

---
+++ Test_PostingImage/Test_PostingImage/C4WorkSpace.h
@@ -0,0 +1,9 @@
+#import "C4CanvasController.h"
+#import "FirstView.h"
+
+@interface C4WorkSpace : C4CanvasController{
+    FirstView *firstView;
+    SecondView *secondView;
+    
+}
+@end
---
+++ Test_PostingImage/Test_PostingImage/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Test_PostingImage
+//
+//  Created by SuseMiessner on 10/25/13.
+//  Copyright (c) 2013 SuseMiessner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([C4AppDelegate class]));
+    }
+}
