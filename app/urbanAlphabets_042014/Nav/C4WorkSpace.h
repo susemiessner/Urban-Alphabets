5d871893ab17cc27ed3021efcd9fac292247309e
susemiessner <susanne.miessner@gmail.com>
2014-04-05 20:12:07 +0300

    change in logic: rootviewcontroller is now the Alphabetview

---
+++ app/urbanAlphabets_042014/Nav/C4WorkSpace.h
@@ -0,0 +1,52 @@
+//
+//  C4WorkSpace.h
+//  Nav
+//
+//  Created by moi on 12/5/2013.
+//
+
+#import "C4CanvasController.h"
+#import <CoreLocation/CoreLocation.h>
+#import "BottomNavBar.h"
+#import "AlphabetMenu.h"
+
+@interface C4WorkSpace : UIViewController<UITextViewDelegate,CLLocationManagerDelegate /**/>
+
+@property (nonatomic) BottomNavBar *bottomNavBar;
+
+//defaults
+@property (readwrite) NSMutableArray *currentAlphabet; //UIImageViews
+@property (readwrite) NSMutableArray *currentAlphabetUIImage;
+@property (readwrite) NSString *currentLanguage;
+@property (readwrite) NSMutableArray *languages;
+@property (readwrite) NSString *oldLanguage;
+@property (readwrite) NSString *alphabetName;
+@property (readwrite) NSString *userName;
+@property (readwrite) NSMutableArray *myAlphabets;
+@property (readwrite) NSMutableArray *myAlphabetsLanguages;
+
+//languages
+@property (readwrite)NSArray *finnish;
+@property (readwrite)NSArray *german;
+@property(readwrite)NSArray *danish;
+@property(readwrite)NSArray *english;
+@property (readwrite)NSArray *spanish;
+@property (readwrite)NSArray *russian;
+
+
+
+
+
+-(void)setup;
+-(void)loadDefaultAlphabet;
+-(NSString *)documentsDirectory;
+-(void)writeAlphabetsUserDefaults;
+-(void)loadCorrectAlphabet;
+-(void)goToMyAlphabets;
+
+
+@property (nonatomic) AlphabetMenu *menu;
+@property (readwrite) int letterTouched;
+@property (readwrite, strong) UIImage *currentAlphabetImage;
+@property (readwrite, strong) UIImage *currentAlphabetImageAsUIImage;
+@end
