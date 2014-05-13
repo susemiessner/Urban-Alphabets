ae6268e63ccaf6cb6ddd15b888753ba035baa7bb
susemiessner <susanne.miessner@gmail.com>
2014-05-13 13:34:52 +0300

    Riga Screen+ Projection (1920x1080 and 1024x768)

---
+++ UrbanScreens/Riga_projection/src/Alphabet.h
@@ -0,0 +1,89 @@
+//
+//  Alphabet.h
+//  Riga01
+//
+//  Created by Suse on 09/05/14.
+//
+//
+
+#ifndef Riga01_Alphabet_h
+#define Riga01_Alphabet_h
+
+class AlphabetEntry{
+public:
+    int _id;
+    string _letter;
+    ofImage _image;
+    
+    int _xPos=1024;
+    int _yPos=170;
+    int _offset=277+15;
+    int _constNo;
+    
+    //constructor
+    AlphabetEntry(string THEID, string LETTER, int constructorNumber){
+        _id=ofToInt(THEID);
+        _letter=LETTER;
+        _constNo=constructorNumber;
+        if(constructorNumber<5){
+            //  printf("now");
+            _xPos=_xPos+(constructorNumber)*_offset;
+        }
+        
+        // printf("inital xpos: %i\n",_xPos);
+        
+    }
+    void print(){
+        printf("id     %i ",_id);
+        printf("letter %s \n",_letter.c_str());
+        
+    }
+    void loadImage(){
+        string URL="http://www.ualphabets.com/images/244x200/"+ofToString(_id)+".png";
+        // printf("%s \n", URL.c_str());
+        ofHttpResponse resp=ofLoadURL(URL);
+        _image.loadImage(resp);
+        //printf("letter: %s \n", _letter.c_str());
+    }
+    void loadImageDirectory(){
+        string path="letters/letter_";
+        path+=ofToString(_letter);
+        path+=".png";
+        // printf("path: %s \n", path.c_str());
+        _image.loadImage(path);
+    }
+    
+    void draw(){
+        ofSetColor(255);
+        //printf("xPos %i ", _xPos);
+        _image.draw(_xPos,_yPos,277, 400);
+    }
+    void drawWhole(){
+        int width=85;
+        int height=122;
+        int spacing=7;
+        int noOfColumns=11;
+        int column=_constNo % noOfColumns;
+        int myXPos=10+column*(width+spacing);
+        
+        int myYPos=100+(_constNo-column)/noOfColumns*(height+spacing);
+        //ofSetColor(255);
+        _image.draw(myXPos,myYPos,width ,height);
+    }
+    void update(){
+        _xPos--;
+    }
+    bool nextImage(){
+        //printf("___ %i::", _xPos);
+        if(_xPos<-435){
+            _xPos=1024;
+            _yPos=2000;
+            return true;
+        } else{
+            return false;
+        }
+    }
+};
+
+
+#endif
---
+++ UrbanScreens/Riga_projection/src/Letter.h
@@ -0,0 +1,55 @@
+#//
+//  Letter.h
+//  Riga01
+//
+//  Created by Suse on 09/05/14.
+//
+//
+
+#ifndef Riga01_Letter_h
+#define Riga01_Letter_h
+
+class Letter{
+public:
+    int _id;
+    ofImage _image;
+    string _owner;
+    string _letter;
+    
+    int _xPos=18;
+    int _yPos=200;
+    int _offset=185+16;
+    int _constNumber;
+    
+    Letter(string THEID, string LETTER, string OWNER, int constructorNumber){
+        _id=ofToInt(THEID);
+        _owner=OWNER;
+        _letter=LETTER;
+        _constNumber=constructorNumber;
+        if(constructorNumber<5){
+            _xPos=_xPos+(constructorNumber)*_offset;
+        }
+    }
+    void print(){
+        printf("id     %i ",_id);
+        printf("letter %s ",_letter.c_str());
+        printf("owner %s \n",_owner.c_str());
+    }
+    void loadImage(){
+        string URL="http://www.ualphabets.com/images/original/"+ofToString(_id)+".png";
+        ofHttpResponse resp=ofLoadURL(URL);
+        _image.loadImage(resp);
+    }
+    
+    void draw(){
+        //ofSetColor(255);
+        ofRect(_xPos-1, _yPos-1, 187, 269);
+        _image.draw(_xPos,_yPos,185, 267);
+    }
+    void update(){
+        //_xPos--;
+    }
+};
+
+
+#endif
