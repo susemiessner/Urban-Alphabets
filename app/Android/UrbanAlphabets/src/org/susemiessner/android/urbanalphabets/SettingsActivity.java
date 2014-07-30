c297f86fbf07a7e62738312ec6c61592711f1f5a
pokhrel <saurabhpokhrel@Saurabhs-MacBook-Pro.local>
2014-07-30 18:17:08 +0300

    additions2

--- app/Android/UrbanAlphabets/src/org/susemiessner/android/urbanalphabets/SettingsActivity.java
+++ app/Android/UrbanAlphabets/src/org/susemiessner/android/urbanalphabets/SettingsActivity.java
@@ -1,14 +1,21 @@
 package org.susemiessner.android.urbanalphabets;
 
 import android.support.v7.app.ActionBarActivity;
+import android.widget.TextView;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class SettingsActivity extends ActionBarActivity {
 
+	private TextView textViewUsername;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_settings);
+		textViewUsername = (TextView)findViewById(R.id.textview_username);
+		SharedPreferences mSharedPreferences = getPreferences(MODE_PRIVATE);
+		
+		textViewUsername.setText(mSharedPreferences.getString("username", "defvalue"));
 	}
 
 
