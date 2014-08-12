3d667ed566a8793094f639d8f282682d5a6b16dc
susemiessner <susemiessner>
2014-08-12 09:51:08 +0300

    helsinki screen - version 01

---
+++ UrbanScreens/Helsinki_1024x512/src/Alphabet.h
@@ -0,0 +1,99 @@
+//
+//  Alphabet.h
+//  Riga01
+//
+//  Created by Suse on 09/05/14.
+//
+//
+
+#ifndef Riga01_Alphabet_h
+#define Riga01_Alphabet_h
+
+class AlphabetEntry{
+public:
+    int _id;
+    string _letter;
+    ofImage _image;
+    
+    int _xPos=1024;
+    int _yPos=80;
+    int _offset=200+45;
+    int _constNo;
+    
+    //constructor
+    AlphabetEntry(string THEID, string LETTER, int constructorNumber){
+        _id=ofToInt(THEID);
+        _letter=LETTER;
+        _constNo=constructorNumber;
+        if(constructorNumber<5){
+            //  printf("now");
+            _xPos=_xPos+(constructorNumber)*_offset;
+        }
+        
+        // printf("inital xpos: %i\n",_xPos);
+        
+    }
+    void print(){
+        printf("id     %i ",_id);
+        printf("letter %s \n",_letter.c_str());
+        
+    }
+    void loadImage(){
+        string identifier=ofToString(_id);
+        string folderName=ofToString(identifier.at(0))+ofToString(identifier.at(1));
+        if (_id<1000) {
+            folderName=ofToString(identifier.at(0));
+        }
+        string URL="http://www.ualphabets.com/images/244x200/"+folderName+"/"+ofToString(_id)+".png";
+        //printf("%s \n", URL.c_str());
+        ofHttpResponse resp=ofLoadURL(URL);
+        _image.loadImage(resp);
+        //printf("letter: %s \n", _letter.c_str());
+    }
+    void loadImageDirectory(){
+        string path="letters/letter_";
+        if (ofToString(_letter)=="?") {
+            path+="-";
+        }else{
+            path+=ofToString(_letter);
+        }
+        path+=".png";
+        
+        // printf("path: %s \n", path.c_str());
+        _image.loadImage(path);
+    }
+    
+    void draw(){
+        ofSetColor(255);
+        //printf("xPos %i ", _xPos);
+        _image.draw(_xPos,_yPos, 200, 244);
+    }
+    void drawWhole(){
+        int width=82;
+        int height=100;
+        int spacing=10;
+        int noOfColumns=11;
+        int column=_constNo % noOfColumns;
+        int myXPos=12+column*(width+spacing);
+        
+        int myYPos=40+(_constNo-column)/noOfColumns*(height+spacing);
+        //ofSetColor(255);
+        _image.draw(myXPos,myYPos,width ,height);
+    }
+    void update(){
+        _xPos--;
+    }
+    bool nextImage(){
+        //printf("___ %i::", _xPos);
+        if(_xPos<-200){
+            _xPos=1024;
+            _yPos=2000;
+            return true;
+        } else{
+            return false;
+        }
+    }
+};
+
+
+#endif
---
+++ UrbanScreens/Helsinki_1024x512/src/Postcard.h
@@ -0,0 +1,64 @@
+//
+//  Postcard.h
+//  Riga01
+//
+//  Created by Suse on 08/05/14.
+//
+//
+
+#ifndef Riga01_Postcard_h
+#define Riga01_Postcard_h
+
+class Postcard{
+public:
+    int _id;
+    ofImage _image;
+    float _longi;
+    float _lati;
+    string _text;
+    string _owner;
+    
+    int _xPos=328;
+    int _yPos=20;
+    int _offset=342+32;
+    
+    Postcard(string THEID, string LONGI, string LA, string TEXT, string OWNER, int constructorNumber){
+        _id=ofToInt(THEID);
+        _longi=ofToFloat( LONGI);
+        _lati=ofToFloat(LA);
+        _text=TEXT;
+        _owner=OWNER;
+
+    }
+    void print(){
+        printf("id     %i ",_id);
+        printf("longi  %f ",_longi);
+        printf("lati   %f ",_lati);
+        printf("letter %s ",_text.c_str());
+        printf("owner %s \n",_owner.c_str());
+        
+    }
+    void loadImage(){
+        string identifier=ofToString(_id);
+        string folderName=ofToString(identifier.at(0))+ofToString(identifier.at(1));
+        if (_id<1000) {
+            folderName=ofToString(identifier.at(0));
+        }
+        string URL="http://www.ualphabets.com/images/original/"+folderName+"/"+ofToString(_id)+".png";
+        ofHttpResponse resp=ofLoadURL(URL);
+        _image.loadImage(resp);
+    }
+    
+    void draw(){
+        //ofSetColor(255);
+        ofRect(_xPos-1, _yPos-1, 370, 450);
+        _image.draw(_xPos,_yPos,368, 448);
+    }
+    void update(){
+        //_xPos--;
+    }
+};
+
+
+
+#endif
