049a54ac2af9a747087f19589bf561eb5aa8ea72
pokhrel <saurabhpokhrel@gmail.com>
2014-08-14 20:36:25 +0300

    welcome activity modified

--- app/Android/UrbanAlphabets/src/org/susemiessner/android/urbanalphabets/WelcomeActivity.java
+++ app/Android/UrbanAlphabets/src/org/susemiessner/android/urbanalphabets/WelcomeActivity.java
@@ -4,15 +4,30 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class WelcomeActivity extends Activity {
+	private int click;
+	private ImageView imageView;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_welcome);
+		imageView = (ImageView)findViewById(R.id.imageview_welcome);
+		click = 0;
+		imageView.setImageResource(R.drawable.intro_1);
 	}
 	
 	public void onClick(View v) {
+		click++;
+		if(click == 1) {
+			imageView.setImageResource(R.drawable.intro_2);
+			return;
+		}
+		else if(click == 2) {
+			imageView.setImageResource(R.drawable.intro_3);
+			return;
+		}
 		Intent mainIntent = new Intent(this, MainActivity.class);
 		startActivity(mainIntent);
 		finish();
