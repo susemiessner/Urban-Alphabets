28f7f92d701f9dbe656c71b91fc6e9f5b6cbc3f1
susemiessner <mail@susemiessner.org>
2014-09-03 11:00:24 +0300

    RigaPlasmaScreenNew release build
    - changed to fullscreen mode

--- UrbanScreens/Riga_PlasmaScreen_new/src/main.cpp
+++ UrbanScreens/Riga_PlasmaScreen_new/src/main.cpp
@@ -3,7 +3,7 @@
 
 //========================================================================
 int main( ){
-    ofSetupOpenGL(1920,1080,OF_WINDOW);
+    ofSetupOpenGL(1920,1080,OF_FULLSCREEN);
     
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
